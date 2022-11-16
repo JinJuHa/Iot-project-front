a49e738e2863d90eb92e19758b50123386b2e2ff
daramjiyun <315ljy@gmail.com>
2022-11-16 23:25:54 +0900

    try

--- src/services/socketio.service.js
+++ src/services/socketio.service.js
@@ -5,7 +5,7 @@ class SocketioService {
   constructor() {}
 
   setupSocketConnection() {
-    this.socket = io(process.env.VUE_APP_SOCKET_ENDPOINT);
+    this.socket = io.connect('http://localhost:8002');
 
     this.socket.emit('my message', 'Hello there from Vue.');
 
